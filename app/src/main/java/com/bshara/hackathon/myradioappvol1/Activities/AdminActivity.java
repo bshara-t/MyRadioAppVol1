d3ed58306e31d9196ae9c805b60a7265385cee3c
bshara-t <bshara2704@gmail.com>
2019-03-12 19:38:59 +0200

    first commit the pakeges and the activities

---
+++ app/src/main/java/com/bshara/hackathon/myradioappvol1/Activities/AdminActivity.java
@@ -0,0 +1,15 @@
+package com.bshara.hackathon.myradioappvol1.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bshara.hackathon.myradioappvol1.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
---
+++ app/src/main/java/com/bshara/hackathon/myradioappvol1/GuestActivity.java
@@ -0,0 +1,13 @@
+package com.bshara.hackathon.myradioappvol1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest);
+    }
+}
